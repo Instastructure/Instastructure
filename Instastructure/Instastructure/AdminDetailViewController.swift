633fdf435427d2c0c37e148a85ea94bfa96b589b
Joel Annenberg <joelannenberg@aol.com>
2016-05-14 15:46:43 -0400

    Updated AdminViewControllers

--- Instastructure/Instastructure/AdminDetailViewController.swift
+++ Instastructure/Instastructure/AdminDetailViewController.swift
@@ -9,27 +9,39 @@
 import UIKit
 
 class AdminDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var stateControl: UISegmentedControl!
+    @IBOutlet weak var noteField: UITextField!
+    @IBOutlet weak var requestImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onUpdatePhoto(sender: AnyObject) {
+        
     }
-    */
-
+    
+    @IBAction func onDelete(sender: AnyObject) {
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Instastructure/Instastructure/AdminViewController.swift
+++ Instastructure/Instastructure/AdminViewController.swift
@@ -7,29 +7,85 @@
 //
 
 import UIKit
+import Parse
 
-class AdminViewController: UIViewController {
-
+class AdminViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var requests: [PFObject]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 320
+        tableView.reloadData()
+        
+        doQuery()
+        
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func doQuery() {
+        // construct PFQuery
+        let query = PFQuery(className: "Request")
+        query.orderByDescending("createdAt")
+        query.includeKey("author")
+        query.limit = 20
+        
+        // fetch data asynchronously
+        query.findObjectsInBackgroundWithBlock { (media: [PFObject]?, error: NSError?) -> Void in
+            if let media = media {
+                self.requests = media
+                self.tableView.reloadData()
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
     }
-    */
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let requests = requests {
+            return requests.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RequestCell", forIndexPath: indexPath) as! RequestCell
+        cell.request = requests![indexPath.row]
+        
+        return cell
+    }
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        doQuery()
+        
+        // Hide the RefreshControl
+        refreshControl.endRefreshing()
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
